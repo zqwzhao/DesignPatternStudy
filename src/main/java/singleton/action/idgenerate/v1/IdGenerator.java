3713cdda5599b41fa976d73bf538e59d73ed1c48
zhaoqw <861143165@qq.com>
2022-01-14 18:14:22 +0800

    IdGenerator Refactor3

--- src/main/java/singleton/action/idgenerate/v1/IdGenerator.java
+++ src/main/java/singleton/action/idgenerate/v1/IdGenerator.java
@@ -21,7 +21,9 @@ public class IdGenerator {
     public long getId() {
         return id.getAndAdd(new Random().nextInt(10));
     }
-
+    public long generate() {
+        return id.getAndAdd(new Random().nextInt(10));
+    }
     public static void main(String[] args) {
         ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(4,8,5, TimeUnit.SECONDS,
                 new ArrayBlockingQueue<>(10),new ThreadPoolExecutor.DiscardOldestPolicy());
