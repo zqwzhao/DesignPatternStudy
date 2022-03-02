3d946b0a33b155695b1bafe30eb7625a6471ceec
zhaoqw <zhaoqw@brains-info.com>
2022-03-02 10:42:26 +0800

    add decorator bridge pattern example

---
+++ src/main/java/bridge/demo1/ApiStatInfo.java
@@ -0,0 +1,10 @@
+package bridge.demo1;
+
+/**
+ * @author: zhaoqw
+ * @date: 2022/3/2 09:55
+ */
+public class ApiStatInfo {
+  private Integer errorCount;
+
+}
---
+++ src/main/java/bridge/demo1/NotificationEmergencyLevel.java
@@ -0,0 +1,54 @@
+package bridge.demo1;
+
+import java.util.List;
+
+public enum NotificationEmergencyLevel {
+  SEVERE, URGENCY, NORMAL, TRIVIAL
+}
+
+class Notification {
+  private List<String> emailAddresses;
+  private List<String> telephones;
+  private List<String> wechatIds;
+
+  public Notification() {}
+
+  public void setEmailAddress(List<String> emailAddress) {
+    this.emailAddresses = emailAddress;
+  }
+
+  public void setTelephones(List<String> telephones) {
+    this.telephones = telephones;
+  }
+
+  public void setWechatIds(List<String> wechatIds) {
+    this.wechatIds = wechatIds;
+  }
+
+  public void notify(NotificationEmergencyLevel level, String message) {
+    if (level.equals(NotificationEmergencyLevel.SEVERE)) {
+      //...自动语音电话
+    } else if (level.equals(NotificationEmergencyLevel.URGENCY)) {
+      //...发微信
+    } else if (level.equals(NotificationEmergencyLevel.NORMAL)) {
+      //...发邮件
+    } else if (level.equals(NotificationEmergencyLevel.TRIVIAL)) {
+      //...发邮件
+    }
+  }
+}
+
+//在API监控告警的例子中，我们如下方式来使用Notification类：
+class ErrorAlertHandler extends AlertHandler {
+  public ErrorAlertHandler(AlertRule rule, Notification notification){
+    super(rule, notification);
+  }
+
+
+  @Override
+  public void check(ApiStatInfo apiStatInfo) {
+//    if (apiStatInfo.getErrorCount() > rule.getMatchedRule(apiStatInfo.getApi()).getMaxErrorCount()) {
+//      notification.notify(NotificationEmergencyLevel.SEVERE, "...");
+//    }
+  }
+}
